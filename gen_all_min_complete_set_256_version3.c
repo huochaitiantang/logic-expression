ee7e38627473a1f78584b92d42719367c0817ec7
liuliang <1432249204@qq.com>
2018-11-21 11:41:22 +0800

    version3: delete one element & gen with not ever appear while C_n_m

--- gen_all_min_complete_set_256_version3.c
+++ gen_all_min_complete_set_256_version3.c
@@ -252,27 +252,68 @@ int has_sub_complete(int * chooses, int chooses_len, Complete* complete, int com
 	return 0;
 }
 
+// get the 0,1,2,...m-1, and which complete set it has been in
+void get_index_complete(int m, Complete* complete, int complete_len, Complete* index_complete){
+    int i, j, cnt = 0;
+    for(i = 0; i < m; i++){
+        index_complete[i].len = 0;
+        index_complete[i].inds = NULL;
+    }
+    for(i = 0; i < m; i++){
+        for(j = 0; j < complete_len; j++){
+            if(inside_array(i, complete[j].len, complete[j].inds))
+                cnt++;
+        }
+        if(cnt > 0){
+            index_complete[i].len = cnt;
+            index_complete[i].inds = (int*)malloc(sizeof(int) * cnt);
+            cnt = 0;
+            for(j = 0; j < complete_len; j++){
+                if(inside_array(i, complete[j].len, complete[j].inds))
+                    index_complete[i].inds[cnt++] = j;
+            }
+        }
+    }
+    return;
+}
+
 // calculate members of C(n,m), res as a array
 int get_complete_C_n_m(int n, int m, Complete* complete, int complete_len, FuncTab* cands, int cands_len){
     int* p;
     int i, j, k = 0, nk = 0;
 	int ind, flag, is_complete;
+    int * complete_flag;
+    Complete* index_complete;
     FuncTab tmp_func;
     FILE *fptr;
-    
+   
+    complete_flag = (int*)malloc(sizeof(int) * complete_len);
+    index_complete = (Complete*)malloc(sizeof(Complete) * m);
     p = (int *)malloc(sizeof(int) * m);
+
+    get_index_complete(m, complete, complete_len, index_complete);
+    for(i = 0; i < complete_len; i++)
+        complete_flag[i] = 0;
+
     ind = 0;
     p[ind] = 0;
+    for(i = 0; i < index_complete[0].len; i++)
+        complete_flag[index_complete[0].inds[i]]++;
     while(1){
         if(p[ind] >= n){
             if(ind == 0) break;
             ind--;
             p[ind]++;
+            //while(p[ind] < n && not_satisify)
+            //    p[ind]++;
         }
         else if(ind == m - 1){
             // one result p = c(n,m)
             // check if their sub set is already complete, if yes jump 
-            flag = has_sub_complete(p, m, complete, complete_len);
+            for(j = 0; j < m ; j++)
+                printf("%d ", p[j]);
+            printf("\n");
+            /*flag = has_sub_complete(p, m, complete, complete_len);
             if(!flag){
             	// debug info
             	k++;
@@ -304,19 +345,66 @@ int get_complete_C_n_m(int n, int m, Complete* complete, int complete_len, FuncT
 					fclose(fptr);
 					complete_len++; 
 				}
-        	}
+        	}*/
             p[ind]++;
+            //while(p[ind] < n && not_satisify)
+            //    p[ind]++;
         }
         else{
             ind++;
             p[ind]=p[ind-1]+1;
+            //while(p[ind] < n && not_satisify)
+            //    p[ind]++;
         }
     }
+    for(i = 0; i < m; i++){
+        if(index_complete[i].inds != NULL)
+            free(index_complete[i].inds);
+    }
+    free(index_complete);
+    free(complete_flag);
     free(p);
     return complete_len;
 }
 
-int get_all_complete(Complete* complete, FuncTab* cands, int cands_len){
+
+void get_one_complete_set_and_delete(FuncTab* cands, int* cands_len){
+    int i, j, tmp, cnt = 0;
+    int* flag;
+    FILE* fptr;
+    int* p;
+    flag = (int*)malloc(sizeof(int) * (*cands_len));
+    p = (int*)malloc(sizeof(int) * 1);
+    fptr = fopen("out.txt", "a");
+    // get complete set
+    for(i = 0; i < (*cands_len); i++){
+        p[0] = i;
+        if(check_complete(cands, (*cands_len), p, 1)){
+            flag[cnt++] = i;
+            fprintf(fptr, " %s %d ", cands[i].ident, cands[i].args_count);
+            for(j = 0; j < cands[i].bits_count; j++)
+                fprintf(fptr, "%d", cands[i].bits[j]);
+            fprintf(fptr, "\n");
+        }
+    }
+    // delete one element in the candiate set
+    for(i = cnt - 1; i >= 0; i--){
+        tmp = flag[i];
+		free(cands[tmp].ident);
+		free(cands[tmp].bits);
+        for(j = tmp + 1; j < (*cands_len); j++)
+            cands[j - 1] = cands[j];
+        *cands_len = *cands_len - 1;
+    }
+    fclose(fptr);
+    free(p);
+    free(flag);
+    return;
+}
+
+
+
+int get_all_complete(Complete* complete, FuncTab* cands, int* cands_len){
 	int i;
 	int complete_len = 0;
 	FILE * fptr;
@@ -324,9 +412,13 @@ int get_all_complete(Complete* complete, FuncTab* cands, int cands_len){
 	fptr = fopen("out.txt", "w");
 	fclose(fptr);
 	
-	for(i = 1; i <= cands_len; i++){
-		printf("Choose %d from %d\n", i, cands_len);
-		complete_len = get_complete_C_n_m(cands_len, i, complete, complete_len, cands, cands_len);
+    // find all 1 function complete set and get rid of it
+    get_one_complete_set_and_delete(cands, cands_len);
+    
+
+	for(i = 2; i < *cands_len; i++){
+		printf("Choose %d from %d\n", i, *cands_len);
+		complete_len = get_complete_C_n_m(*cands_len, i, complete, complete_len, cands, *cands_len);
 		printf("cur complete: %d\n", complete_len);
 	}
 
@@ -353,20 +445,23 @@ void print_complete(Complete* complete, int complete_len, FuncTab* cands){
 int main(){
 	int i;
 	int complete_len;
-	int cands_len = 256;
+	int cands_len = 5;
+    int start_args_cnt = 0;
 	
 	FuncTab* cands;
-    Complete complete[50000];
+    Complete complete[10000];
 	clock_t start, end; 
 	
 	start = clock();
 	//freopen("out.txt", "w", stdout); 
 	cands = (FuncTab*)malloc(sizeof(FuncTab) * cands_len);
-	init_cands(cands, cands_len, 3);
+	init_cands(cands, cands_len, start_args_cnt);
 	print_cands(cands, cands_len);
 
-	complete_len = get_all_complete(complete, cands, cands_len);
-	print_complete(complete, complete_len, cands);
+	complete_len = get_all_complete(complete, cands, &cands_len);
+    print_cands(cands, cands_len);
+
+    print_complete(complete, complete_len, cands);
 	
 	// free space
 	for(i = 0; i < cands_len; i++){
